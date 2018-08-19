ed366f11e01bc95a4a498c51a29d9deb4cbfe6dc
limin <1162322644@qq.com>
2018-08-20 02:41:38 +0800

    注册用户

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        //已勾选记住直接跳到欢迎页
+        if (Auth::user() && Auth::viaRemember()) {
+            return redirect()->route('users.show', [Auth::user()]);
+        }
+
+        return view('sessions.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+//        dd($credentials);
+
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+
+}
